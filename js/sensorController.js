37c6a03c9af61f0f598c7e06e6a752603945ed89
Maxime Paul <maxime.a.paul@gmail.com>
2016-01-05 15:42:22 -0500

    Added initial files

---
+++ js/sensorController.js
@@ -0,0 +1,12 @@
+(function(jQuery, Firebase, Path) {
+    "use strict";
+ //Overall funtions
+
+    // the main firebase reference
+    var rootRef = new Firebase('https://pitchlove.firebaseio.com');
+    
+    
+    
+    
+    
+}(window.jQuery, window.Firebase, window.Path))
